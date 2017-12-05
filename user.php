bb55f0a0ac2c10b538e584b5fc1858fee03d2d3d
A1\pavel.volkov <pavel.volkov@arealsoft.ru>
2017-12-05 11:00:29 +0300

    deleting unnecessary rows

--- user.php
+++ user.php
@@ -2,9 +2,6 @@
 use Bitrix\Main\UserTable;
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
 
-$arFilter = array(
-    "ID" => 2175
-);
 $res = UserTable::getList(array(
     "select" => array(
         "ID",
